07bd1a667ccbbd1ea03125c318a44979545ba5ab
servisofts <servisofts.srl@gmail.com>
2025-09-11 12:15:49 -0400

    v2

---
+++ server/sbin/download_servisofts_java_service_2.sh
@@ -0,0 +1,5 @@
+
+NAME="servisofts-java-service-2.jar"
+# wget https://repo.servisofts.com/home/servisofts/libs/java/servisofts-java-service.jar
+curl -O https://drive.servisofts.com/http/serp/c9caa964-88f3-43db-88df-684ecf5c0a1b/repo/servisofts-java-service-2/$NAME
+# mv ./servisofts-java-service.jar ./lib/servisofts-java-service.jar
